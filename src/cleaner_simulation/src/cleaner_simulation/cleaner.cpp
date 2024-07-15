3e35c6f16baf7b5c51887526a21cbc7d1d8a92b5
wjd123ap <46740963+wjd123ap@users.noreply.github.com>
2024-07-16 02:47:16 +0900

    Update cleaner.cpp

--- src/cleaner_simulation/src/cleaner_simulation/cleaner.cpp
+++ src/cleaner_simulation/src/cleaner_simulation/cleaner.cpp
@@ -1,4 +1,18 @@
+// STL
+#include <iostream>
+#include <mutex>
+#include <string>
+
+
+
+// ROS
+#include <ros/ros.h>
+#include <thread>
+#include <nav_msgs/Odometry.h>
+#include <sensor_msgs/JointState.h>
+
 #include <cmath>
+
 #include "cleaner_simulation/cleaner.hpp"
 Cleaner::Cleaner(){
 
